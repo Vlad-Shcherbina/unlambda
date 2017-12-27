4b39e12808f276217d71e36a706f76b2c06c58f6
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-12-27 14:29:12 +0300

    e function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::rc::Rc;
 use std::io::Write;
 
+// Err(t) means that the computation was prematurely terminated by `et.
+type EvalResult = Result<Rc<Term>, Rc<Term>>;
+
 #[derive(PartialEq, Eq, Debug)]
 enum Term {
     K,
@@ -13,6 +16,7 @@ enum Term {
     D,
     Promise(Rc<Term>),
     Print(char),
+    E,
     Apply(Rc<Term>, Rc<Term>),
 }
 use Term::*;
@@ -30,6 +34,7 @@ impl ToString for Term {
             D => String::from("d"),
             Promise(ref t) => format!("promise({})", t.to_string()),
             Print(c) => if c == '\n' { String::from("r") } else { format!(".{}", c) }
+            E => String::from("e"),
             Apply(ref f, ref x) => format!("`{}{}", f.to_string(), x.to_string()),
         }
     }
@@ -44,6 +49,7 @@ fn parse(it: &mut Iterator<Item=char>) -> Term {
             'i' => I,
             'v' => V,
             'd' => D,
+            'e' => E,
             '.' => Print(it.next().unwrap()),
             'r' => Print('\n'),
             '#' => {
@@ -67,28 +73,28 @@ fn parse_str(s: &str) -> Rc<Term> {
 
 // Never returns Apply(...) term, so eval() is idempotent
 // (second call returns the same value and has no IO side effects).
-fn eval(term: Rc<Term>, io: &mut Write) -> Rc<Term> {
+fn eval(term: Rc<Term>, io: &mut Write) -> EvalResult {
     if let Apply(ref f, ref x) = *term {
-        let f = eval(Rc::clone(f), io);
+        let f = eval(Rc::clone(f), io)?;
         if let D = *f {
-            return Rc::new(Promise(Rc::clone(x)));
+            return Ok(Rc::new(Promise(Rc::clone(x))));
         }
         return apply(
             f,
-            eval(Rc::clone(x), io), io);
+            eval(Rc::clone(x), io)?, io);
     }
-    term
+    Ok(term)
 }
 
 // Never returns Apply(...) term.
-fn apply(f: Rc<Term>, x: Rc<Term>, io: &mut Write) -> Rc<Term> {
+fn apply(f: Rc<Term>, x: Rc<Term>, io: &mut Write) -> EvalResult {
     if let Apply(_, _) = *f {
         panic!();
     }
     if let Apply(_, _) = *x {
         panic!();
     }
-    match *f {
+    Ok(match *f {
         K => Rc::new(K1(x)),
         K1(ref y) => Rc::clone(y),
         S => Rc::new(S1(x)),
@@ -102,7 +108,7 @@ fn apply(f: Rc<Term>, x: Rc<Term>, io: &mut Write) -> Rc<Term> {
         S2(ref y, ref z) =>
             eval(Rc::new(Apply(
                 Rc::new(Apply(Rc::clone(y), Rc::clone(&x))),
-                Rc::new(Apply(Rc::clone(z), Rc::clone(&x))))), io),
+                Rc::new(Apply(Rc::clone(z), Rc::clone(&x))))), io)?,
 
         Print(c) => {
             io.write_fmt(format_args!("{}", c)).unwrap();
@@ -110,20 +116,21 @@ fn apply(f: Rc<Term>, x: Rc<Term>, io: &mut Write) -> Rc<Term> {
         }
         I => x,
         V => Rc::clone(&f),  // TODO: ideally simply move f, but it's borrowed
+        E => return Err(x),
         D => panic!("should be handled in eval"),
 
         // Similarly, apply(eval(f), x) instead of eval(`fx)
         // is probably incorrect. What if f = Promise(D)?
-        Promise(ref f) => eval(Rc::new(Apply(Rc::clone(f), x)), io),
+        Promise(ref f) => eval(Rc::new(Apply(Rc::clone(f), x)), io)?,
 
         _ => unimplemented!("{:?}", f),
+    })
     }
-}
 
 fn main() {
     let program = parse_str("``````````````.H.e.l.l.o.,. .w.o.r.l.d.!rv");
     let t = eval(program, &mut std::io::stdout());
-    assert_eq!(t.to_string(), "v");
+    assert_eq!(t.unwrap().to_string(), "v");
 }
 
 #[cfg(test)]
@@ -139,7 +146,9 @@ mod tests {
 
     fn run_and_expect(program: &str, result: Option<&str>, output: Option<&str>) {
         let mut buf = Vec::<u8>::new();
-        let actual_result = eval(parse_str(program), &mut buf).to_string();
+        let actual_result = eval(parse_str(program), &mut buf)
+            .unwrap_or_else(|e| e)
+            .to_string();
         if let Some(result) = result {
             assert_eq!(&actual_result.to_string(), result);
         }
@@ -170,5 +179,21 @@ mod tests {
         run_and_expect("``dd`ri", None, Some("\n"));
         run_and_expect("``id`ri", None, Some(""));
         run_and_expect("```s`kdri", None, Some(""));
+
+        run_and_expect("``ii`.av", Some("v"), Some("a"));
+        run_and_expect("``ei`.av", Some("i"), Some(""));
+    }
+
+    #[test]
+    fn ramanujan() {
+        // From the documentation
+        let mut expected = "*".repeat(1729);
+        expected.push('\n');
+        run_and_expect("
+        ```s`kr``s``si`k.*`ki
+         ```s``s`k``si`k`s``s`ksk``s``s`ksk``s``s`kski
+           ``s`k``s``s`ksk``s``s`kski`s``s`ksk
+          ```s``s`kski``s``s`ksk``s``s`kski
+        ", None, Some(&expected));
     }
 }
